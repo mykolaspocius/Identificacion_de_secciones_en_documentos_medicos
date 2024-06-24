9f25a8c42e0d22b45ca455468df31d464f66d08a
mykolas <pociusmykolas@hotmail.com>
2024-06-24 13:12:48 +0200

    comments for predicitions.py

--- predictions.py
+++ predictions.py
@@ -1,10 +1,15 @@
+# Code located in this file is used to create predictions using given pretrained model and
+# save tmen in a file using te ClinAISDataset format
+
 from dataset_model import *
 from WordListSplitter import EntrySplitter
 from post_process import *
 from typing import List
 from tqdm import tqdm
 
-
+# This function rescieves a list of lists containing boundary annotations for each part thay the orginal entry is split
+# The process continues until each part is no bigger than length_limit
+# This length limit is calculated using model's tokenizer or it can be preset and sent as parameter
 def split_entry(b_annotations_list:List[List[BoundaryAnnotation]],tokenizer,max_length_preset=None)->List[List[BoundaryAnnotation]]:
     res = []
     rdy = True
@@ -20,13 +25,15 @@ def split_entry(b_annotations_list:List[List[BoundaryAnnotation]],tokenizer,max_
             res.append(b_annotations)
     return rdy,res
 
+# This function returns returns entries's text parts corresponding to each list in ba_splits of boundary annotations
 def get_text_splits(entry:Entry,ba_splits:List[List[BoundaryAnnotation]])->List[str]:
     res = []
     for b_annotations in ba_splits:
         res.append(entry.note_text[b_annotations[0].start_offset:b_annotations[-1].end_offset+1])
     return res
 
-
+# for each text in text_splits makes a prediction using givven model
+# returns a single prediction combinning partial predictions
 def make_prediction(text_splits,model):
     prediction=Prediction()
     offset = 0
@@ -40,6 +47,7 @@ def make_prediction(text_splits,model):
         offset+=len(text_split)
     return prediction
 
+# Applyes post porccessing actions to prediction from the model
 def post_process_prediction(prediction:Prediction,verbuous=True):
     ppp=PredictionPostProcessor(prediction,verbuous)
     ppp.reasign_leading_punctuation_marks()
@@ -47,6 +55,9 @@ def post_process_prediction(prediction:Prediction,verbuous=True):
     ppp.merge_contiguous_equivalent_sections()
 
 
+# Returns subset of boundary annotations given as parameter corresponding to range (start,end)
+# Will include all boudary annotations which have their starting token inside te range
+# Could happen that the end of the last boundary annotation is outside given range
 def getBoudaryAnnotationsForRange(bas:List[BoundaryAnnotation],start,end)->List[BoundaryAnnotation]:
     res = []
     idx = 0
@@ -56,8 +67,11 @@ def getBoudaryAnnotationsForRange(bas:List[BoundaryAnnotation],start,end)->List[
         idx+=1
     return res
 
+# This function takes original boundary annotations from the entry and relabels them
+# so the labbels match with prediction passed as parameter
+# Asigns section label for the first boundary annotation in the list of b annotations correspondig for the section
+# asigns None to the rest
 def create_b_annotations(entry:Entry,prediction:Prediction):
-
     b_annotations_predicted = [BoundaryAnnotation(**ba.__dict__) for ba in entry.boundary_annotation.gold]
     for i,section in enumerate(prediction.sections):
         bas = getBoudaryAnnotationsForRange(b_annotations_predicted,section.start,section.end)
@@ -71,6 +85,7 @@ def create_b_annotations(entry:Entry,prediction:Prediction):
     return b_annotations_predicted
 
 
+# Completes entries prediction sections for section_annotations and boundary_annotations using predictions from the given model
 def process_entry(entry:Entry,model):
     rdy=False
     ba_splits = [entry.boundary_annotation.gold]
@@ -89,7 +104,7 @@ def process_entry(entry:Entry,model):
     entry.boundary_annotation.prediction = b_annotations_predicted
 
 
-
+# create file which can be used to evaluate model predictions using evaluation scripts in ./evaluation
 def create_predictions_file(dataset_path,save_predicted_path,model):
     with open(dataset_path,encoding='utf-8') as f:
         dataset: ClinAISDataset = ClinAISDataset(**json.load(f))  
